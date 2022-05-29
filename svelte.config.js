7bedc95f0649dbeb309b49c6cfbbfb9683b10ef9
NNB <nnbnh@protonmail.com>
2022-05-29 15:18:44 +0000

    deleted:    docs/.nojekyll 	deleted:    docs/_app/assets/bg-2029b318.jpg 	deleted:    docs/_app/assets/pages/__layout.svelte-98c72b7a.css 	deleted:    docs/_app/assets/pages/index.svelte-e0345fa5.css 	deleted:    docs/_app/assets/pages/p__id_.svelte-15aa4d1f.css 	deleted:    docs/_app/chunks/index-7dbdcc9a.js 	deleted:    docs/_app/chunks/index-a71c70ce.js 	deleted:    docs/_app/chunks/language-723e5d43.js 	deleted:    docs/_app/chunks/paths-396f020f.js 	deleted:    docs/_app/chunks/products-0d025528.js 	deleted:    docs/_app/chunks/singletons-d1fb5791.js 	deleted:    docs/_app/chunks/stores-68d8d76e.js 	deleted:    docs/_app/error.svelte-45222ee4.js 	deleted:    docs/_app/manifest.json 	deleted:    docs/_app/pages/__layout.svelte-f9138440.js 	deleted:    docs/_app/pages/bill__info_.svelte-b93558ba.js 	deleted:    docs/_app/pages/index.svelte-04420c9d.js 	deleted:    docs/_app/pages/p__id_.svelte-cc8ed079.js 	deleted:    docs/_app/version.json 	deleted:    docs/bg.jpg 	deleted:    docs/icon.png 	deleted:    docs/index.html 	deleted:    docs/logo.png 	deleted:    docs/paymentmethod/grabpay.jpg 	deleted:    docs/paymentmethod/paypal.jpg 	deleted:    docs/paymentmethod/shopeepay.jpg 	deleted:    docs/paymentmethod/techcombank.jpg 	deleted:    docs/paymentmethod/tpbank.jpg 	deleted:    docs/paymentmethod/viettelpay.jpg 	deleted:    docs/producer.svg 	deleted:    docs/showcase.jpg 	modified:   package-lock.json 	modified:   package.json 	modified:   svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -4,11 +4,10 @@ import preprocess from 'svelte-preprocess';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: preprocess(),
-
 	kit: {
 		adapter: adapter({
-			pages: 'docs',
-			assets: 'docs',
+			pages: 'build',
+			assets: 'build',
 			fallback: 'index.html'
 		}),
 		paths: {
