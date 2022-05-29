eed564f62af55be6c597618f1732f2ec04b061ca
NNB <nnbnh@protonmail.com>
2022-05-29 15:18:36 +0000

    Updates

---
+++ _app/chunks/language-723e5d43.js
@@ -0,0 +1,1 @@
+import{w as e}from"./index-a71c70ce.js";const l="en";var a;const o=(a=window.localStorage.getItem("lang"))!=null?a:l,n=e(o);n.subscribe(t=>{window.localStorage.setItem("lang",t)});export{n as l};
