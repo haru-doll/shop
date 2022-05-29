ff576f5161a88d9860e218e0d4275777c58ce759
NNB <nnbnh@protonmail.com>
2022-05-29 16:25:10 +0000

    modified:   svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -11,7 +11,7 @@ const config = {
 			fallback: 'index.html'
 		}),
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/haru-doll.github.io' : ''
+			base: process.env.NODE_ENV === 'production' ? '/web' : ''
 		}
 	}
 };
