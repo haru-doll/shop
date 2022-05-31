495561dd2624db5156207d89933a09ad705c7367
NNB <nnbnh@protonmail.com>
2022-05-31 14:14:19 +0000

    deleted:    static/icon.png 	modified:   svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -8,8 +8,7 @@ const config = {
 		adapter: adapter({
 			pages: 'build',
 			assets: 'build',
-			fallback: null,
-			precompress: false
+			fallback: null
 		}),
 		paths: {
 			base: process.env.NODE_ENV === 'production' ? '/shop' : ''
