f3eb24f9c1efbcc6802f744b25fbb5d6bae7c2c2
NNB <nnbnh@protonmail.com>
2022-06-01 00:40:28 +0000

    Updates

---
+++ _app/immutable/chunks/language-58a2e4f6.js
@@ -0,0 +1,1 @@
+import{w as e}from"./index-4f8d8095.js";const l="en";var a;const o=(a=window.localStorage.getItem("lang"))!=null?a:l,n=e(o);n.subscribe(t=>{window.localStorage.setItem("lang",t)});export{n as l};
