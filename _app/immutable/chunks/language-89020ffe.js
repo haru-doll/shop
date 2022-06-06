dc5f46fd06d26f7e88c5aa4833176aad1228b8f1
NNB <nnbnh@protonmail.com>
2022-06-06 17:42:23 +0000

    Updates

--- _app/immutable/chunks/language-89020ffe.js
+++
@@ -1,1 +0,0 @@
-import{w as e}from"./paths-18cf2cfa.js";const l="en";var a;const o=(a=window.localStorage.getItem("lang"))!=null?a:l,n=e(o);n.subscribe(t=>{window.localStorage.setItem("lang",t)});export{n as l};
---
+++ _app/immutable/chunks/language-f3262e3c.js
@@ -0,0 +1,1 @@
+import{w as e}from"./paths-2ea04750.js";const l="en";var a;const o=(a=window.localStorage.getItem("lang"))!=null?a:l,n=e(o);n.subscribe(t=>{window.localStorage.setItem("lang",t)});export{n as l};
